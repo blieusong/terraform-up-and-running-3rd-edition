48db9433069d007e488b5be406281fa05f5f498c
Benh LIEU SONG <benh.lieusong@gmail.com>
2023-06-28 21:57:03 +0100

    web server + variable + output

--- chapter 01/main.tf
+++ chapter 01/main.tf
@@ -5,8 +5,38 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami = "ami-090b049bea4780001"
     instance_type = "t4g.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
 
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World!" > index.html
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF
+
+    user_data_replace_on_change = true
     tags = {
         Name = "terraform-example"
     }
+}
+
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+
+    ingress {
+        from_port = var.server_port
+        to_port = var.server_port
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+variable "server_port" {
+    description = "Port the server will use for http requests"
+    type = number
+    default = 8080
+}
+
+output "public_ip" {
+    value = aws_instance.example.public_ip
+    description = "Public IP address of the web server"  
 }
