33bb5a84deb35b254535870cfededb7575025a91
Benh LIEU SONG <benh.lieusong@gmail.com>
2023-06-29 22:28:32 +0100

    autoscaling group

--- chapter 01/main.tf
+++ chapter 01/main.tf
@@ -1,42 +1,67 @@
 provider "aws" {
-    region = "eu-west-1"
+  region = "eu-west-1"
 }
 
-resource "aws_instance" "example" {
-    ami = "ami-090b049bea4780001"
-    instance_type = "t4g.micro"
-    vpc_security_group_ids = [aws_security_group.instance.id]
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-090b049bea4780001"
+  instance_type   = "t4g.micro"
+  security_groups = [aws_security_group.instance.id]
 
-    user_data = <<-EOF
+  user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World!" > index.html
                 nohup busybox httpd -f -p ${var.server_port} &
                 EOF
+  # required when using a launch configuration with an auto scaling group
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-    user_data_replace_on_change = true
-    tags = {
-        Name = "terraform-example"
-    }
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnets.default.ids
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+
 }
 
 resource "aws_security_group" "instance" {
-    name = "terraform-example-instance"
+  name = "terraform-example-instance"
 
-    ingress {
-        from_port = var.server_port
-        to_port = var.server_port
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 variable "server_port" {
-    description = "Port the server will use for http requests"
-    type = number
-    default = 8080
+  description = "Port the server will use for http requests"
+  type        = number
+  default     = 8080
 }
 
 output "public_ip" {
-    value = aws_instance.example.public_ip
-    description = "Public IP address of the web server"  
+  value       = aws_launch_configuration.example.associate_public_ip_address
+  description = "Public IP address of the web server"
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
