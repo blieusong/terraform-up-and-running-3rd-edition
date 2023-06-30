1062d59fbe19055b3434eea3668e77076684d1cd
Benh LIEU SONG <benh.lieusong@gmail.com>
2023-06-30 18:48:06 +0100

    Added working load balancer example - work only on the dev AWS instance - some discrepancies with the book because of the VPC settings

--- chapter 01/main.tf
+++ chapter 01/main.tf
@@ -20,7 +20,10 @@ resource "aws_launch_configuration" "example" {
 
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
-  vpc_zone_identifier = data.aws_subnets.default.ids
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
 
   min_size = 2
   max_size = 10
@@ -30,11 +33,11 @@ resource "aws_autoscaling_group" "example" {
     value               = "terraform-asg-example"
     propagate_at_launch = true
   }
-
 }
 
 resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
+  name   = "terraform-example-instance"
+  vpc_id = data.aws_vpc.default.id
 
   ingress {
     from_port   = var.server_port
@@ -50,18 +53,103 @@ variable "server_port" {
   default     = 8080
 }
 
-output "public_ip" {
-  value       = aws_launch_configuration.example.associate_public_ip_address
-  description = "Public IP address of the web server"
+output "alb_dns_name" {
+  value       = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
 }
 
 data "aws_vpc" "default" {
-  default = true
+  filter {
+    name   = "tag:Name"
+    values = ["Default VPC"]
+  }
 }
 
 data "aws_subnets" "default" {
   filter {
-    name = "vpc-id"
+    name   = "vpc-id"
     values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_lb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+output "default_subnets_ids" {
+  value       = data.aws_subnets.default.ids
+  description = "List of default subnets"
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name   = "terraform-example-alb"
+  vpc_id = data.aws_vpc.default.id
+
+  # allow inbound HTTP requests
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # allow all outbound requests
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
   }
 }
